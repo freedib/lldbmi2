4ab1c2df3f1233083b14a7d0ca841784110ed457
freedib <diblibre@gmail.com>
2016-04-09 12:17:05 -0400

    Updated build system to allow variable number of arguments to cmake
    Allow a non standard liblldb.dylib with -DUSE_LIB_PATH arg to cmake

--- build.sh
+++ build.sh
@@ -2,5 +2,13 @@
 mkdir build
 mkdir logs
 cd build
-/usr/local/bin/cmake .. $1 $2
-make $3
+# separates cmake options from make tag which is last
+# OPT = -DCMAKE_BUILD_TYPE=Debug -DBUILD_TESTS=1 -DUSE_LIB_PATH=/lldb_tree/debug/lib  ...
+# TAG = all, clean
+OPT=
+TAG=
+for F; do OPT="$OPT $TAG"; TAG=$F; done
+# create cmake tree
+/usr/local/bin/cmake .. $OPT
+# build
+make $TAG
